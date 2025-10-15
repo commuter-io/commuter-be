8b410b8a2682c6d558565bdc3801397276e4c3a2
does-soobinPark <soobin.park@does.kr>
2025-10-16 00:27:33 +0900

    application.yml 깃에서 제거

---
+++ src/main/java/org/example/backend/domain/Station.java
@@ -0,0 +1,33 @@
+package org.example.backend.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Station extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; // 역 이름
+
+    @Column(nullable = false)
+    private String lineNumber; // 호선
+
+    @Column(unique = true)
+    private String stationCode; // 외부 API 연동을 위한 역 코드
+}
